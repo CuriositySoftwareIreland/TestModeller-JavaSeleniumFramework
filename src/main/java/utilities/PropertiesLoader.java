a667e48978eed1af94bc8bc9efc9c1814e3ddc9e
Andrey Ustalakov <austalakov@gmail.com>
2019-11-19 18:34:27 +0000

    Generate test cases on failure

--- src/main/java/utilities/PropertiesLoader.java
+++ src/main/java/utilities/PropertiesLoader.java
@@ -5,13 +5,11 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesLoader {
-    private static Properties prop = new Properties();
-
-    private static boolean propertiesLoaded = false;
+    private static Properties prop;
 
     public static Properties getProperties()
     {
-        if (!propertiesLoaded) {
+        if (prop == null) {
             try {
                 PropertiesLoader.readProperties();
             } catch (IOException e) {
@@ -25,11 +23,16 @@ public class PropertiesLoader {
 
     private static void readProperties() throws IOException
     {
+        Properties defaults = new Properties();
+        defaults.setProperty("testModeller.uploadResults", "true");
+        defaults.setProperty("testModeller.analyseFailures", "false");
+        defaults.setProperty("testModeller.jobTimeout", "30000");
+
+        prop = new Properties(defaults);
+
         try (InputStream inputStream = ClassLoader.getSystemResourceAsStream("project.properties"))
         {
             prop.load(inputStream);
-
-            propertiesLoaded = true;
         }
     }
 }
--- src/main/java/utilities/testmodeller/TestNGListener.java
+++ src/main/java/utilities/testmodeller/TestNGListener.java
@@ -1,5 +1,9 @@
 package utilities.testmodeller;
 
+import ie.curiositysoftware.jobengine.services.job.TestGenerationService;
+import ie.curiositysoftware.jobengine.utils.JobExecutor;
+import ie.curiositysoftware.testmodeller.TestModellerSuite;
+import org.testng.*;
 import utilities.PropertiesLoader;
 import ie.curiositysoftware.jobengine.services.ConnectionProfile;
 import ie.curiositysoftware.runresult.dto.TestPathRun;
@@ -7,116 +11,125 @@ import ie.curiositysoftware.runresult.dto.TestPathRunStatusEnum;
 import ie.curiositysoftware.runresult.services.TestRunIdGenerator;
 import ie.curiositysoftware.runresult.services.TestRunService;
 import ie.curiositysoftware.testmodeller.TestModellerPath;
-import org.testng.IClass;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static java.lang.Math.toIntExact;
 
-public class TestNGListener implements ITestListener {
+public class TestNGListener implements ITestListener, IClassListener {
 
-    @Override
-    public void onFinish(ITestContext test) {
-        System.out.println("Test Case finished" + test.getName());
-    }
+    private TestRunService runService;
+    private TestGenerationService generationService;
+    private JobExecutor jobExecutor;
 
-    @Override
-    public void onStart(ITestContext test) {
-        System.out.println("Test Case started" + test.getName());
-    }
+    private int failedTestsInClass = 0;
+    private List<ITestClass> testsClassesToAnalyse = new ArrayList<>();
 
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+    private final boolean uploadResults;
+    private final boolean analyseFailures;
+    private final long jobTimeout;
 
-    }
+    public TestNGListener() {
+        uploadResults = Boolean.parseBoolean(PropertiesLoader.getProperties().getProperty("testModeller.uploadResults"));
+        analyseFailures = Boolean.parseBoolean(PropertiesLoader.getProperties().getProperty("testModeller.analyseFailures"));
+        jobTimeout = Long.parseLong(PropertiesLoader.getProperties().getProperty("testModeller.jobTimeout"));
 
-    @Override
-    public void onTestFailure(ITestResult testResult) {
-        // Get guid
-        String guid = GetTestModellerPathGuid(testResult);
+        String apiHost = PropertiesLoader.getProperties().getProperty("testModeller.apiHost");
+        String apiKey = PropertiesLoader.getProperties().getProperty("testModeller.apiKey");
+        ConnectionProfile profile = new ConnectionProfile(apiHost, apiKey);
 
-        if (guid == null)
-            return;
-
-        // Create TestPath run entity
-        TestPathRun testPathRun = new TestPathRun();
-        testPathRun.setMessage(testResult.getThrowable().getMessage());
-        testPathRun.setRunTime(toIntExact(testResult.getEndMillis() - testResult.getStartMillis()));
-        testPathRun.setRunTimeStamp(new Date(testResult.getStartMillis()));
-        testPathRun.setTestPathGuid(guid);
-        testPathRun.setVipRunId(TestRunIdGenerator.getRunId());
-        testPathRun.setTestStatus(TestPathRunStatusEnum.Failed);
-
-        // Post it
-        TestRunService runService = new TestRunService(new ConnectionProfile(PropertiesLoader.getProperties().getProperty("testModeller.apiHost"), PropertiesLoader.getProperties().getProperty("testModeller.apiKey")));
-        if (!runService.saveTestPathRun(testPathRun)) {
-            System.out.println("Error posting path run " + runService.getErrorMessage());
+        if (uploadResults) {
+            runService = new TestRunService(profile);
+        }
+        if(analyseFailures) {
+            generationService = new TestGenerationService(profile);
+            jobExecutor = new JobExecutor(profile);
         }
     }
 
     @Override
-    public void onTestSkipped(ITestResult arg0) {
-
-    }
-
-    @Override
-    public void onTestStart(ITestResult arg0) {
-        System.out.println("Test Case started" + arg0);
+    public void onTestFailure(ITestResult testResult) {
+        System.out.println("Test failure");
+        failedTestsInClass++;
+        if(uploadResults)
+            postRunResult(testResult, TestPathRunStatusEnum.Failed);
     }
 
     @Override
     public void onTestSuccess(ITestResult testResult) {
         System.out.println("Test success");
+        if(uploadResults)
+            postRunResult(testResult, TestPathRunStatusEnum.Passed);
+    }
 
+    @Override
+    public void onBeforeClass(ITestClass testClass) {
+        failedTestsInClass = 0;
+    }
+
+    @Override
+    public void onAfterClass(ITestClass testClass) {
+        if(analyseFailures && failedTestsInClass > 0)
+            testsClassesToAnalyse.add(testClass);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        testsClassesToAnalyse.forEach(this::postAnalysisJob);
+    }
+
+    private void postRunResult(ITestResult testResult, TestPathRunStatusEnum status) {
         // Get guid
-        String guid = GetTestModellerPathGuid(testResult);
-
-        if (guid == null)
+        TestModellerPath path = getTestModellerPath(testResult);
+        if (path == null)
             return;
 
         // Create TestPath run entity
         TestPathRun testPathRun = new TestPathRun();
         testPathRun.setRunTime(toIntExact(testResult.getEndMillis() - testResult.getStartMillis()));
         testPathRun.setRunTimeStamp(new Date(testResult.getStartMillis()));
-        testPathRun.setTestPathGuid(guid);
+        testPathRun.setTestPathGuid(path.guid());
         testPathRun.setVipRunId(TestRunIdGenerator.getRunId());
-        testPathRun.setTestStatus(TestPathRunStatusEnum.Passed);
+        testPathRun.setTestStatus(status);
+        if(testResult.getThrowable() != null)
+            testPathRun.setMessage(testResult.getThrowable().getMessage());
 
         // Post it
-        TestRunService runService = new TestRunService(new ConnectionProfile(PropertiesLoader.getProperties().getProperty("testModeller.apiHost"), PropertiesLoader.getProperties().getProperty("testModeller.apiKey")));
         if (!runService.saveTestPathRun(testPathRun)) {
             System.out.println("Error posting path run " + runService.getErrorMessage());
         }
     }
 
-    private String GetTestModellerPathGuid(ITestResult testResult)
-    {
-        String pathGuid = null;
-
-        IClass obj = testResult.getTestClass();
-
-        Class newobj = obj.getRealClass();
-
-        Method testMethod = null;
-        try {
-            testMethod = newobj.getMethod(testResult.getName());
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+    private void postAnalysisJob(ITestClass testClass) {
+        TestModellerSuite suite = getTestModellerSuite(testClass);
+        if(suite != null && suite.profileId() > 0) {
+            generationService.startAnalysisAndGenerationJob(suite.profileId());
         }
+    }
 
-        if (testMethod != null && testMethod.isAnnotationPresent(TestModellerPath.class))
-        {
-            TestModellerPath useAsTestName = testMethod.getAnnotation(TestModellerPath.class);
-
-            pathGuid = useAsTestName.guid();
-
-            System.out.println("Test Modeller ID = " + pathGuid);
+    private TestModellerPath getTestModellerPath(ITestResult testResult) {
+        Method testMethod = testResult.getMethod().getConstructorOrMethod().getMethod();
+        if (testMethod != null && testMethod.isAnnotationPresent(TestModellerPath.class)) {
+            TestModellerPath path = testMethod.getAnnotation(TestModellerPath.class);
+            System.out.println("Test Modeller Path GUID = " + path.guid());
+            return path;
+        } else {
+            return null;
         }
+    }
 
-        return pathGuid;
+    private TestModellerSuite getTestModellerSuite(ITestClass testClass) {
+        Class<?> realClass = testClass.getRealClass();
+        if(realClass != null && realClass.isAnnotationPresent(TestModellerSuite.class)) {
+            TestModellerSuite suite = realClass.getAnnotation(TestModellerSuite.class);
+            System.out.println("Test Modeller Suite ID = " + suite.id());
+            return suite;
+        } else {
+            return null;
+        }
     }
 }
