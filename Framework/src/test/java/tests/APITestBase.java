8014b0ccf1dbc96b4a5f330e8213536b93e9fed0
TestFlip <james@Testflip.co.uk>
2022-11-22 19:42:38 +0000

    Create APITestBase.java

---
+++ Framework/src/test/java/tests/APITestBase.java
@@ -0,0 +1,90 @@
+package tests;
+
+import ie.curiositysoftware.allocation.dto.AllocationType;
+import ie.curiositysoftware.allocation.engine.DataAllocation;
+import ie.curiositysoftware.allocation.engine.DataAllocationEngine;
+import ie.curiositysoftware.jobengine.services.ConnectionProfile;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import utilities.CapabilityLoader;
+import utilities.PropertiesLoader;
+import utilities.reports.ExtentReportManager;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class APITestBase {
+    protected ConnectionProfile cp = new ConnectionProfile(PropertiesLoader.getProperties().getProperty("testModeller.apiHost"), PropertiesLoader.getProperties().getProperty("testModeller.apiKey"));
+
+    protected DataAllocationEngine dataAllocationEngine = new DataAllocationEngine(cp);
+
+    public WebDriver getDriver()
+    {
+        return CapabilityLoader.getDriver();
+    }
+
+    @BeforeSuite(alwaysRun = true)
+    public void setupReporter()
+    {
+        ExtentReportManager.setupReporter();
+    }
+
+    @BeforeSuite(alwaysRun = true)
+    public void allocateData(ITestContext testContext)
+    {
+        // Create a list of all the pools that need allocating
+        List<AllocationType> allocationTypes = new ArrayList<AllocationType>();
+
+        ITestNGMethod[] methods =  testContext.getAllTestMethods();
+        try {
+
+            for (int i = 0; i < methods.length; i++) {
+                ITestNGMethod method = methods[i];
+
+                Method testMethod = method.getConstructorOrMethod().getMethod();
+
+                if (testMethod != null && testMethod.isAnnotationPresent(DataAllocation.class))
+                {
+                    DataAllocation dataAllocation = testMethod.getAnnotation(DataAllocation.class);
+
+                    for (String testType : dataAllocation.groups()) {
+                        AllocationType allocationType = new AllocationType(dataAllocation.poolName(), dataAllocation.suiteName(), testType);
+
+                        allocationTypes.add(allocationType);
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            System.err.println(e);
+        }
+
+        // Publish and allocate data
+        if (!dataAllocationEngine.resolvePools(PropertiesLoader.getProperties().getProperty("testModeller.serverName"), allocationTypes)) {
+            System.out.println("Error - " + dataAllocationEngine.getErrorMessage());
+        }
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void initDriver(Method method)
+    {
+        ExtentReportManager.createNewTest(method);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closerDriver()
+    {
+
+    }
+
+    @AfterSuite
+    public void closeReporter()
+    {
+        ExtentReportManager.closeReporter();
+    }
+}
