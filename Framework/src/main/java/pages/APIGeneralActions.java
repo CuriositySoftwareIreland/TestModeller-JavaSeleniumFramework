9b3413f79f5a3a565125acffe4894e48fd104734
TestFlip <james@Testflip.co.uk>
2023-03-20 09:42:02 +0000

    Add API assertions

--- Framework/src/main/java/pages/APIGeneralActions.java
+++ Framework/src/main/java/pages/APIGeneralActions.java
@@ -3,6 +3,8 @@ package pages;
 import com.jayway.jsonpath.JsonPath;
 import ie.curiositysoftware.testmodeller.TestModellerIgnore;
 import io.restassured.response.Response;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 
 public class APIGeneralActions extends BasePage {
@@ -18,11 +20,11 @@ public class APIGeneralActions extends BasePage {
     {
         try {
             String value = JsonPath.read(rsp.getBody().asString(), jsonPath).toString();
-            passStep("Extracted value '" + value + "'");
+            passStep(rsp, "Extracted value '" + value + "'");
 
             return value;
         } catch (Exception e) {
-            failStep("Unable to extract json path '" + jsonPath + "' from response " + rsp.getBody().asString());
+            failStep(rsp, "Unable to extract json path '" + jsonPath + "' from response " + rsp.getBody().asString() + ". " + e.getMessage());
         }
 
         return "";
@@ -42,7 +44,9 @@ public class APIGeneralActions extends BasePage {
     public void AssertStatusCode(Response rsp, int statusCode)
     {
         if (rsp.getStatusCode() != statusCode) {
-            failStep("Status code invalid - expected " + statusCode + " - found " + rsp.getStatusCode());
+            failStep(rsp, "Status code invalid - expected " + statusCode + " - found " + rsp.getStatusCode());
+        } else {
+            passStep(rsp, "Status code '" + statusCode + "'");
         }
     }
 
@@ -51,8 +55,78 @@ public class APIGeneralActions extends BasePage {
      */
     public void AssertJsonPath(Response rsp, String jsonPath, String value)
     {
-        if (!(JsonPath.read(rsp.getBody().asString(), jsonPath).toString().equals(value))) {
-            failStep("Body didn't contain value '" + value + "' at path '" + jsonPath + "'");
+        try {
+            if (!(JsonPath.read(rsp.getBody().asString(), jsonPath).toString().equals(value))) {
+                failStep(rsp, "Body didn't contain value '" + value + "' at path '" + jsonPath + "'");
+            } else {
+                passStep(rsp, "Body contained value '" + value + "' at path '" + jsonPath + "'");
+            }
+        } catch (Exception e) {
+            failStep(rsp, "Unable to extract json path '" + jsonPath + "' from response " + rsp.getBody().asString() + ". " + e.getMessage());
+        }
+    }
+
+    /**
+     * @name Assert Response Contains Header
+     */
+    public void AssertHeaderContains(Response rsp, String headerKey)
+    {
+        if (rsp.getHeaders().get(headerKey) == null) {
+            failStep(rsp, "Response does not contain a header '" + headerKey + "'.");
+        } else {
+            passStep(rsp, "Response contains header '" + headerKey + "'");
+        }
+    }
+
+    /**
+     * @name Assert Response Contains Header with Value
+     */
+    public void AssertHeaderValue(Response rsp, String headerKey, String headerValue)
+    {
+        if (rsp.getHeaders().get(headerKey) == null) {
+            failStep(rsp, "Response does not contain a header '" + headerKey + "'.");
+        } else {
+            if (rsp.getHeaders().get(headerKey).getValue().equals(headerKey)) {
+                passStep(rsp, "API Request header value for '" + headerKey + "' is '" + headerKey + "'.");
+            } else {
+                failStep(rsp, "The API Request header value for '" + headerKey + "' is '" + rsp.getHeaders().get(headerKey).getValue() + "'. Expected '" + headerValue + "'.");
+            }
+        }
+    }
+
+    /**
+     * @name Assert Response JSON Path Exists
+     */
+    public void AssertJSONPathExists(Response rsp, String jsonPath)
+    {
+        try {
+            if (!(JsonPath.read(rsp.getBody().asString(), jsonPath) != null)) {
+                failStep(rsp, "Body does not contain the JSON path '" + jsonPath  + "'.");
+            } else {
+                passStep(rsp, "Body contains the JSON path '" + jsonPath + "'.");
+            }
+        } catch (Exception e) {
+            failStep(rsp, "Unable to extract json path '" + jsonPath + "' from response " + rsp.getBody().asString() + ". " + e.getMessage());
+        }
+    }
+
+    /**
+     * @name Assert JSON Data
+     */
+    public void AssertJSONData(Response rsp)
+    {
+        try {
+            new JSONObject(rsp.getBody().asString());
+
+            passStep(rsp, "The JSON data is valid.");
+        } catch (Exception ex) {
+            try {
+                new JSONArray(rsp.getBody().asString());
+
+                passStep(rsp, "The JSON data is valid.");
+            } catch (Exception ex1) {
+                failStep(rsp, "An exception occured while parsing the JSON result: " + ex1.getMessage());
+            }
         }
     }
 }
