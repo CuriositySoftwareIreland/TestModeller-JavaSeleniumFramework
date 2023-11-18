3291d435eda54bfada10f50373a78fb5981a8845
TestFlip <james@Testflip.co.uk>
2023-11-18 10:40:50 +0000

    Update CapabilityLoader.java

--- Framework/src/main/java/utilities/CapabilityLoader.java
+++ Framework/src/main/java/utilities/CapabilityLoader.java
@@ -180,6 +180,7 @@ public class CapabilityLoader
         options.addArguments("--allow-running-insecure-content");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
 
         options.merge(getDesiredCapabilities());
 
