3b82a3734903b858def98be0e38209bf2573c5e2
TestFlip <james@Testflip.co.uk>
2023-12-14 10:21:03 +0000

    Update APITestBase.java

--- Framework/src/test/java/tests/APITestBase.java
+++ Framework/src/test/java/tests/APITestBase.java
@@ -4,6 +4,7 @@ import ie.curiositysoftware.allocation.dto.AllocationType;
 import ie.curiositysoftware.allocation.engine.DataAllocation;
 import ie.curiositysoftware.allocation.engine.DataAllocationEngine;
 import ie.curiositysoftware.jobengine.services.ConnectionProfile;
+import ie.curiositysoftware.runresult.dto.TestPathRun;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestNGMethod;
@@ -11,18 +12,24 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import pages.VisualAutomation.VisualActions;
+import reports.TestNGListener;
 import utilities.CapabilityLoader;
 import utilities.PropertiesLoader;
 import utilities.reports.ExtentReportManager;
+import utilities.testmodeller.TestModellerLogger;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class APITestBase {
     protected ConnectionProfile cp = new ConnectionProfile(PropertiesLoader.getProperties().getProperty("testModeller.apiHost"), PropertiesLoader.getProperties().getProperty("testModeller.apiKey"));
 
     protected DataAllocationEngine dataAllocationEngine = new DataAllocationEngine(cp);
+
+    protected static HashMap<String, TestPathRun> testRunMap = new HashMap<String, TestPathRun>();
 
     public WebDriver getDriver()
     {
@@ -49,15 +56,21 @@ public class APITestBase {
 
                 Method testMethod = method.getConstructorOrMethod().getMethod();
 
-                if (testMethod != null && testMethod.isAnnotationPresent(DataAllocation.class))
-                {
-                    DataAllocation dataAllocation = testMethod.getAnnotation(DataAllocation.class);
+                if (testMethod != null) {
+                    if (testMethod.isAnnotationPresent(DataAllocation.class)) {
+                        DataAllocation dataAllocation = testMethod.getAnnotation(DataAllocation.class);
 
-                    for (String testType : dataAllocation.groups()) {
-                        AllocationType allocationType = new AllocationType(dataAllocation.poolName(), dataAllocation.suiteName(), testType);
+                        for (String testType : dataAllocation.groups()) {
+                            AllocationType allocationType = new AllocationType(dataAllocation.poolName(), dataAllocation.suiteName(), testType);
 
-                        allocationTypes.add(allocationType);
+                            allocationTypes.add(allocationType);
+                        }
                     }
+
+                    // Store test path run
+                    TestPathRun r = TestNGListener.StartTestRunInQueue(testMethod);
+                    String methodKey = testMethod.getDeclaringClass().getName() + "." + testMethod.getName();
+                    testRunMap.put(methodKey, r);
                 }
             }
         } catch (Throwable e) {
@@ -74,12 +87,29 @@ public class APITestBase {
     public void initDriver(Method method)
     {
         ExtentReportManager.createNewTest(method);
+
+        String methodKey = method.getDeclaringClass().getName() + "." + method.getName();
+        TestModellerLogger.CurrentRun.set(testRunMap.get(methodKey));
+
+        TestNGListener.StartTestRun(method);
     }
 
     @AfterMethod(alwaysRun = true)
     public void closerDriver()
     {
+        System.out.println("Finished test: " + ExtentReportManager.getTestName());
 
+        if (!PropertiesLoader.isDebug()) {
+            try {
+                if (CapabilityLoader.getDriver() != null)
+                    CapabilityLoader.getDriver().quit();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Close all apps open through visual inspector
+        VisualActions.closeApplication();
     }
 
     @AfterSuite
